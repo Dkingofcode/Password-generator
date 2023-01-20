43fedbb7029b8b1afa77d9ccb1fce7865e5a7da0
Dkingofcode <doladepo128@gmail.com>
2023-01-20 00:01:27 +0000

    list steps and added generate-password function

---
+++ app.py
@@ -0,0 +1,15 @@
+# Ask user if they want to generate password
+# if yes, ask for password
+# generate password
+# Print password
+# IOf initial response was no, exit program 
+import string
+import random
+
+def generate_password():
+    password_length = int(input(""))
+
+
+
+
+
